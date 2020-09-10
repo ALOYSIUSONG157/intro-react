c8f607855acacc2dbfdda6a4ba91dbdffabe66c7
Aloysius <macabregg@gmailcom>
2020-09-10 10:17:59 +0800

    5th

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,14 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
+  addStudent(studentName) {
+    this.setState({
+      studentName: this.state.studentName.concat(studentName)
+    });
+  }
 
+
+  
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
     let assignmentName = assignment;
